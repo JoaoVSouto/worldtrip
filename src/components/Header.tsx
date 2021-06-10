083725aa90a04f38a41a85d44d102da99cf028f0
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-10 19:29:38 -0300

    :lipstick: Adapt header component to internal pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,28 @@
-import { Flex, Img } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Flex, Icon, Img, Link as ChakraLink } from '@chakra-ui/react';
+import { FaChevronLeft } from 'react-icons/fa';
 
 export default function Header() {
+  const { pathname } = useRouter();
+
+  const shouldNavigateBackAppear = pathname !== '/';
+
   return (
-    <Flex as="header" justify="center" align="center" py={[4, 5, 7]}>
+    <Flex
+      as="header"
+      justify="center"
+      align="center"
+      pos="relative"
+      py={[4, 5, 7]}
+    >
+      {shouldNavigateBackAppear && (
+        <Link href="/" passHref>
+          <ChakraLink pos="absolute" left={4}>
+            <Icon as={FaChevronLeft} fontSize={14} />
+          </ChakraLink>
+        </Link>
+      )}
       <Img src="/img/logo.svg" alt="world trip" w={[20, 32, 48]} />
     </Flex>
   );
---
+++ src/pages/eu.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+import Header from 'components/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Europa | worldtrip</title>
+      </Head>
+
+      <Header />
+
+      <h1>europe!</h1>
+    </>
+  );
+}
