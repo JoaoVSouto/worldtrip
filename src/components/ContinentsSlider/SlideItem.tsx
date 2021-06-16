211748d847eefe803e8d4998e6ce1bba144466c5
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-16 20:09:42 -0300

    :sparkles: Add links to internal pages on slide items

--- src/components/ContinentsSlider/SlideItem.tsx
+++ src/components/ContinentsSlider/SlideItem.tsx
@@ -1,4 +1,5 @@
-import { Image, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+import { Box, Image, Text } from '@chakra-ui/react';
 
 type ImageData = {
   src: string;
@@ -13,35 +14,45 @@ type SlideItemProps = {
 
 export default function SlideItem({ description, img, title }: SlideItemProps) {
   return (
-    <>
-      <Image
-        className="swiper-lazy"
-        src={img.src}
-        alt={img.alt}
-        objectFit="cover"
+    <Link href="/europa" passHref>
+      <Box
+        as="a"
         h="100%"
         w="100%"
-        position="absolute"
-        css={{ filter: 'brightness(0.3)' }}
-        zIndex="-1"
-      />
-      <Text
-        as="strong"
-        color="gray.50"
-        fontWeight="bold"
-        fontSize={{ base: '2xl', md: '3xl', lg: '6xl' }}
-        mt={12}
+        d="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
       >
-        {title}
-      </Text>
-      <Text
-        color="gray.200"
-        fontWeight="bold"
-        fontSize={{ base: 'sm', md: 'xl', lg: '2xl' }}
-        mt={{ base: 9, md: 4, lg: 4 }}
-      >
-        {description}
-      </Text>
-    </>
+        <Image
+          className="swiper-lazy"
+          src={img.src}
+          alt={img.alt}
+          objectFit="cover"
+          h="100%"
+          w="100%"
+          position="absolute"
+          css={{ filter: 'brightness(0.3)' }}
+          zIndex="-1"
+        />
+        <Text
+          as="strong"
+          color="gray.50"
+          fontWeight="bold"
+          fontSize={{ base: '2xl', md: '3xl', lg: '6xl' }}
+          mt={12}
+        >
+          {title}
+        </Text>
+        <Text
+          color="gray.200"
+          fontWeight="bold"
+          fontSize={{ base: 'sm', md: 'xl', lg: '2xl' }}
+          mt={{ base: 9, md: 4, lg: 4 }}
+        >
+          {description}
+        </Text>
+      </Box>
+    </Link>
   );
 }
